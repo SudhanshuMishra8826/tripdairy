01a2b62553fcc92563b6ee2417a15f3cfbc5a833
sudhanshu <sudhanshu.mishra.8826@gmail.com>
2020-04-21 19:56:57 +0530

    final commit

--- js/main.js
+++ js/main.js
@@ -26,8 +26,10 @@ $(function() {
     forceHeight: false
   });
   var text = ["Perfect solution to manage all conversations between your customers and AI","One stop AI powered communication platform for hospitality industry"];
+  var text2 = ["Tripdairy manages all the hassles with travel for you as your personal travel concierge.","We understand you, your preferences and needs to help you get things done proactively on demand.","Personal Travel Assistant on demand"];
   var counter = 0;
   var elem = $("#postertext");
+  var elem2 = $("#postertext2");  
   setInterval(change, 5000);
   function change() {
       elem.fadeOut(function(){
@@ -35,6 +37,12 @@ $(function() {
           counter++;
           if(counter >= text.length) { counter = 0; }
           elem.fadeIn();
+      });
+          elem2.fadeOut(function(){
+          elem2.html(text2[counter]);
+          counter++;
+          if(counter >= text2.length) { counter = 0; }
+          elem2.fadeIn();
       });
   }
   //------- mailchimp --------//  
